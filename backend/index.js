f9cab504a8f83df21c2f4c323749052a784d66a3
racheldesa <racheldesa@outlook.com>
2024-05-01 12:03:24 -0500

    Implemented GET by stuID

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,22 @@ app.get("/grades", async (req,res) => {
     try {
         const query = "SELECT * FROM grades";
         const [result] = await db.query(query); // Execute the query and wait for the result
-        console.log("Success in Reading MySQL");
         res.status(200).send(result); // Send the results as the response
+    } catch (err) {
+        console.error("Error in Reading MySQL: ", err);
+        res.status(500).send({ error: 'An error occurred while fetching items.' });
+    }
+});
+
+// Fetch grades by student ID
+app.get("/grades/:id", async (req,res) => {
+    try {
+        // Read id from frontend
+        const id = req.params.id;
+        const query = "SELECT * FROM grades WHERE stuID = ?";
+        const [result] = await db.query(query, [id]);
+        console.log("Success in Reading MySQL");
+        res.status(200).send(result);
     } catch (err) {
         console.error("Error in Reading MySQL: ", err);
         res.status(500).send({ error: 'An error occurred while fetching items.' });
