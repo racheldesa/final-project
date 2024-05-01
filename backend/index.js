c461df1437feffa6099bea34020eb9f21880323c
racheldesa <racheldesa@outlook.com>
2024-05-01 11:56:45 -0500

    added route to access grades table in cycourse db

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,16 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
+
+// Route to get all grades from all students
+app.get("/grades", async (req,res) => {
+    try {
+        const query = "SELECT * FROM grades";
+        const [result] = await db.query(query); // Execute the query and wait for the result
+        console.log("Success in Reading MySQL");
+        res.status(200).send(result); // Send the results as the response
+    } catch (err) {
+        console.error("Error in Reading MySQL: ", err);
+        res.status(500).send({ error: 'An error occurred while fetching items.' });
+    }
+});
