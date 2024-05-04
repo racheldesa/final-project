3251d9fe7b19b1840822d30b526d64fb2f9dc8f9
racheldesa <racheldesa@outlook.com>
2024-05-04 10:31:18 -0500

    Update AllCourses.js
    Started working on generate Form (select courses) on All Courses page

--- frontend/src/AllCourses.js
+++ frontend/src/AllCourses.js
@@ -1,11 +1,30 @@
 function AllCourses() {
+    fetch('http://localhost:4000/catalog')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            generateForm(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+
+    function generateForm(data) {
+        let mainContainer = document.getElementById("root");
+        for (let course of data) {
+            console.log(course);
+        }
+            
+    
+    } // end generateForm
 
     return (
         <div>
             <h2>All Courses</h2>
             <div>
                 <form>
-                    
+
                 </form>
             </div>
         </div>
