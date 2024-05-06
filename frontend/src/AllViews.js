762746a2f76295a8a06db381fda439488f4db947
racheldesa <racheldesa@outlook.com>
2024-05-06 09:46:34 -0500

    Added functionality to nav menu

--- frontend/src/AllViews.js
+++ frontend/src/AllViews.js
@@ -5,17 +5,37 @@ export function AllViews() {
     const [allCoursesView, setAllCoursesView] = useState(false);
     const [gradesView, setGradesView] = useState(false);
 
-    const setHome = () => {
-        homeView === false ? setHomeView(true) : setHomeView(false);
-    }
+    // const setHome = () => {
+    //     homeView === false ? setHomeView(true) : setHomeView(false);
+    // }
 
-    const setCourses = () => {
-        allCoursesView === false ? setAllCoursesView(true) : setAllCoursesView(false);
-    }
+    // const setCourses = () => {
+    //     allCoursesView === false ? setAllCoursesView(true) : setAllCoursesView(false);
+    // }
 
-    const setGrades = () => {
-        gradesView === false ? setGradesView(true) : setGradesView(false);
-    }
+    // const setGrades = () => {
+    //     gradesView === false ? setGradesView(true) : setGradesView(false);
+    // }
+
+    const homePageBtn = document.getElementById("homepageBtn");
+    const coursesPageBtn = document.getElementById("coursespageBtn");
+    const gradesPageBtn = document.getElementById("gradespageBtn");
+
+    homePageBtn.addEventListener("click", function () {
+        setHomeView(true);
+        setAllCoursesView(false);
+        setGradesView(false);
+    });
+    coursesPageBtn.addEventListener("click", function () {
+        setHomeView(false);
+        setAllCoursesView(true);
+        setGradesView(false);
+    });
+    gradesPageBtn.addEventListener("click", function () {
+        setHomeView(false);
+        setAllCoursesView(false);
+        setGradesView(true);
+    });
 
     function Home() {
         return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import AllViews from './AllViews';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <AllViews />
