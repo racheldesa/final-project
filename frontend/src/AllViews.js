3b7f06e639b3987eaf174ac7de63f5c7c792d1b8
racheldesa <racheldesa@outlook.com>
2024-05-06 21:17:17 -0500

    Added stuID input to Grades page.

--- frontend/src/AllViews.js
+++ frontend/src/AllViews.js
@@ -1,25 +1,15 @@
 import React, { useState, useEffect } from "react";
+import Grades from "./Grades";
 
 export function AllViews() {
     const [homeView, setHomeView] = useState(true);
     const [allCoursesView, setAllCoursesView] = useState(false);
     const [gradesView, setGradesView] = useState(false);
 
-    // const setHome = () => {
-    //     homeView === false ? setHomeView(true) : setHomeView(false);
-    // }
-
-    // const setCourses = () => {
-    //     allCoursesView === false ? setAllCoursesView(true) : setAllCoursesView(false);
-    // }
-
-    // const setGrades = () => {
-    //     gradesView === false ? setGradesView(true) : setGradesView(false);
-    // }
-
     const homePageBtn = document.getElementById("homepageBtn");
     const coursesPageBtn = document.getElementById("coursespageBtn");
     const gradesPageBtn = document.getElementById("gradespageBtn");
+    const logo = document.getElementById("pageLogo");
 
     homePageBtn.addEventListener("click", function () {
         setHomeView(true);
@@ -36,8 +26,39 @@ export function AllViews() {
         setAllCoursesView(false);
         setGradesView(true);
     });
+    logo.addEventListener("click", function () {
+        setHomeView(true);
+        setAllCoursesView(false);
+        setGradesView(false);
+    });
 
     function Home() {
+        fetch("http://localhost:4000/catalog")
+            .then(response => response.json())
+            .then(catalog => loadCourses(catalog));
+
+        function loadCourses(catalog) {
+            var mainContainer = document.getElementById("main-container");
+            // for (let course of catalog) {
+            //     console.log(course);
+            // }
+            for (let i=0; i<catalog.length; i++) {
+                console.log(`${catalog[i].title}`);
+                let AddCourseCard = document.createElement("div");
+                AddCourseCard.classList.add("row");
+
+                AddCourseCard.innerHTML = `
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <p class="card-text">${catalog[i].title}</p>
+                        </div>
+                    </div>`;
+
+                // mainContainer.appendChild(AddCourseCard);
+            }
+            
+            console.log(catalog.length);
+        }
         return (
             <div>
                 <h1>This is the homepage.</h1>
@@ -53,13 +74,13 @@ export function AllViews() {
         )
     }
 
-    function Grades() {
-        return (
-            <div> 
-                <h1>This is the grades viewing page.</h1>
-            </div>
-        )
-    }
+    // function Grades() {
+    //     return (
+    //         <div> 
+    //             <h1>This is the grades viewing page.</h1>
+    //         </div>
+    //     )
+    // }
 
     return (
         <div>
--- frontend/src/Grades.js
+++ frontend/src/Grades.js
@@ -1,8 +1,32 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
 function Grades() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [dataF,setDataF] = useState({});
+    const [viewer,setViewer] = useState(0);
+
+    function GetID() {
+        const onSubmit = data => {
+            setDataF(data);
+            setViewer(1);
+            console.log(data);
+        }
+        return (
+            <div>
+                <h1>Enter your Student ID:</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("stuID", { required: true })} placeholder="Student ID" />
+                    {errors.stuID && <p>Student ID is required.</p>}
+                    <button type="submit">Go</button>
+                </form>
+            </div>
+        );
+    } // end GetID()
 
     return (
         <div>
-
+            {viewer===0 && <GetID />}
         </div>
     );
 }
