48e6593d1587b4e82780521e50c572fcdb7affe9
Luke Pedersen <lukeped@iastate.edu>
2024-05-08 11:08:45 -0500

    update AllCourses.js

--- frontend/src/AllCourses.js
+++ frontend/src/AllCourses.js
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-
-
-
 function AllCourses() {
-    fetch('http://localhost:4000/catalog')
-    .then(response => response.json())
-    .then(myCourses => loadCourses(myCourses));
-    
-    function loadCourses(data) {
-        var mainContainer = document.getElementById("main-container");
-        let div = document.createElement("div");
-        // Read all course options from response
-        for (let course of data) {
-            div.innerHTML += `
-                <input class="form-check-input" type="checkbox" value="" id="${course.title}>"`
-        }
-    }
-    
+    fetch("http://localhost:4000/catalog")
+        .then(response => response.json())
+        .then(catalog => loadCourses(catalog));
 
+    function loadCourses(catalog) {
+        var mainContainer = document.getElementById("main-container");
+        for (let course of catalog) {
+            console.log(course);
+        }
+        for (let i=0; i<catalog.length; i++) {
+            console.log(`${catalog[i].title}`);
+            let card = "course" + i.toString();
+            let AddCourseCard = document.createElement("div");
+            AddCourseCard.classList.add("course-container");
+
+            AddCourseCard.innerHTML = `
+                <div id=${card} class="card shadow-sm">
+                    <div class="card-body">
+                        <h2 class="card-text">${catalog[i].title}</h2>
+                        <p class="card-text">Professor: ${catalog[i].professor}</p>
+                        <p class="card-text">Credits: ${catalog[i].credits}</p>
+                        <p class="card-text">${catalog[i]. courseTag} | ${catalog[i]. sessions}</p>
+                    </div>
+                </div>`;
+
+            mainContainer.appendChild(AddCourseCard);
+        }
+        
+        console.log(catalog.length);
+    }
     return (
         <div>
-            <h2>All Courses</h2>
-            <div>
-                <form>
-                    <button type="submit">Update</button>
-                </form>
-            </div>
+            <h1>Course Catalog</h1>
+            <div id="main-container"></div>
         </div>
-    );
+    )
 }
 export default AllCourses;
--- frontend/src/AllViews.js
+++ frontend/src/AllViews.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Grades from "./Grades";
+import AllCourses from "./AllCourses";
 
 export function AllViews() {
     const [homeView, setHomeView] = useState(true);
@@ -36,47 +37,6 @@ export function AllViews() {
         return (
             <div>
                 <h1>This is the homepage.</h1>
-            </div>
-        )
-    }
-
-    function AllCourses() {
-        fetch("http://localhost:4000/catalog")
-            .then(response => response.json())
-            .then(catalog => loadCourses(catalog));
-
-        function loadCourses(catalog) {
-            var mainContainer = document.getElementById("main-container");
-            for (let course of catalog) {
-                console.log(course);
-            }
-            for (let i=0; i<catalog.length; i++) {
-                console.log(`${catalog[i].title}`);
-                let card = "course" + i.toString();
-                let AddCourseCard = document.createElement("div");
-                AddCourseCard.classList.add("course-container");
-
-                AddCourseCard.innerHTML = `
-                    <div id=${card} class="card shadow-sm">
-                        <div class="card-body">
-                            <h1 class="card-text">${catalog[i].title}</h1>
-                            <p class="card-text">Professor: ${catalog[i].professor}</p>
-                            <p class="card-text">Credits: ${catalog[i].credits}</p>
-                            <p class="card-text">${catalog[i]. courseTag} | ${catalog[i]. sessions}</p>
-                        </div>
-                    </div>`;
-
-                mainContainer.appendChild(AddCourseCard);
-
-                // let ccard = document.getElementById(card);
-                // cards.push(ccard);
-            }
-            
-            console.log(catalog.length);
-        }
-        return (
-            <div>
-                <div id="main-container"></div>
             </div>
         )
     }
