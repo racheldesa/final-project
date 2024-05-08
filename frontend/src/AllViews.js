10544933b1dbe17149506556fe9c7daf3ccce418
lukeped <lukeped@iastate.edu>
2024-05-08 08:46:55 -0500

    established course cards

--- frontend/src/AllViews.js
+++ frontend/src/AllViews.js
@@ -33,32 +33,6 @@ export function AllViews() {
     });
 
     function Home() {
-        fetch("http://localhost:4000/catalog")
-            .then(response => response.json())
-            .then(catalog => loadCourses(catalog));
-
-        function loadCourses(catalog) {
-            var mainContainer = document.getElementById("main-container");
-            // for (let course of catalog) {
-            //     console.log(course);
-            // }
-            for (let i=0; i<catalog.length; i++) {
-                console.log(`${catalog[i].title}`);
-                let AddCourseCard = document.createElement("div");
-                AddCourseCard.classList.add("row");
-
-                AddCourseCard.innerHTML = `
-                    <div class="card shadow-sm">
-                        <div class="card-body">
-                            <p class="card-text">${catalog[i].title}</p>
-                        </div>
-                    </div>`;
-
-                // mainContainer.appendChild(AddCourseCard);
-            }
-            
-            console.log(catalog.length);
-        }
         return (
             <div>
                 <h1>This is the homepage.</h1>
@@ -67,20 +41,54 @@ export function AllViews() {
     }
 
     function AllCourses() {
+        fetch("http://localhost:4000/catalog")
+            .then(response => response.json())
+            .then(catalog => loadCourses(catalog));
+
+        function loadCourses(catalog) {
+            var mainContainer = document.getElementById("main-container");
+            for (let course of catalog) {
+                console.log(course);
+            }
+            for (let i=0; i<catalog.length; i++) {
+                console.log(`${catalog[i].title}`);
+                let card = "course" + i.toString();
+                let AddCourseCard = document.createElement("div");
+                AddCourseCard.classList.add("course-container");
+
+                AddCourseCard.innerHTML = `
+                    <div id=${card} class="card shadow-sm">
+                        <div class="card-body">
+                            <h1 class="card-text">${catalog[i].title}</h1>
+                            <p class="card-text">Professor: ${catalog[i].professor}</p>
+                            <p class="card-text">Credits: ${catalog[i].credits}</p>
+                            <p class="card-text">${catalog[i]. courseTag} | ${catalog[i]. sessions}</p>
+                        </div>
+                    </div>`;
+
+                mainContainer.appendChild(AddCourseCard);
+
+                // let ccard = document.getElementById(card);
+                // cards.push(ccard);
+            }
+            
+            console.log(catalog.length);
+        }
         return (
             <div>
                 <h1>This is the course selection page.</h1>
+                <div id="main-container"></div>
             </div>
         )
     }
 
-    // function Grades() {
-    //     return (
-    //         <div> 
-    //             <h1>This is the grades viewing page.</h1>
-    //         </div>
-    //     )
-    // }
+    function Grades() {
+        return (
+            <div> 
+                <h1>This is the grades viewing page.</h1>
+            </div>
+        )
+    }
 
     return (
         <div>
