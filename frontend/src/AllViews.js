4a69146c8022f9c71888700f80a6f58e91a924f0
lukeped <lukeped@iastate.edu>
2024-05-09 01:20:49 -0500

    included student information page

--- frontend/src/AllViews.js
+++ frontend/src/AllViews.js
@@ -3,17 +3,20 @@ import Grades from "./Grades";
 import AllCourses from "./AllCourses";
 import Home from "./Homepage";
 import AdminView from "./AdminView";
+import DeveloperInfo from "./DeveloperInfo";
 
 export function AllViews() {
     const [homeView, setHomeView] = useState(true);
     const [allCoursesView, setAllCoursesView] = useState(false);
     const [gradesView, setGradesView] = useState(false);
     const [adminView, setAdminView] = useState(false);
+    const [developerView, setDeveloperView] = useState(false);
 
     const homePageBtn = document.getElementById("homepageBtn");
     const coursesPageBtn = document.getElementById("coursespageBtn");
     const gradesPageBtn = document.getElementById("gradespageBtn");
     const adminPageBtn = document.getElementById("adminpageBtn");
+    const developerPageBtn = document.getElementById("developerpageBtn");
     const logo = document.getElementById("pageLogo");
 
 
@@ -22,30 +25,42 @@ export function AllViews() {
         setAllCoursesView(false);
         setGradesView(false);
         setAdminView(false);
+        setDeveloperView(false);
     });
     coursesPageBtn.addEventListener("click", function () {
         setHomeView(false);
         setAllCoursesView(true);
         setGradesView(false);
         setAdminView(false);
+        setDeveloperView(false);
     });
     gradesPageBtn.addEventListener("click", function () {
         setHomeView(false);
         setAllCoursesView(false);
         setGradesView(true);
         setAdminView(false);
+        setDeveloperView(false);
     });
     logo.addEventListener("click", function () {
         setHomeView(true);
         setAllCoursesView(false);
         setGradesView(false);
         setAdminView(false);
+        setDeveloperView(false);
     });
     adminPageBtn.addEventListener("click", function () {
         setHomeView(false);
         setAllCoursesView(false);
         setGradesView(false);
         setAdminView(true);
+        setDeveloperView(false);
+    });
+    developerPageBtn.addEventListener("click", function(){
+        setHomeView(false);
+        setAllCoursesView(false);
+        setGradesView(false);
+        setAdminView(false);
+        setDeveloperView(true);
     });
 
     return (
@@ -54,6 +69,7 @@ export function AllViews() {
             {allCoursesView && <AllCourses />}
             {gradesView && <Grades />}
             {adminView && <AdminView />}
+            {developerView && <DeveloperInfo />}
         </div>
     );
 }
---
+++ frontend/src/DeveloperInfo.js
@@ -0,0 +1,44 @@
+function DeveloperInfo() {
+    return (
+        <div id="main-container">
+            <h1>SE/ComS319 Construction of User Interfaces, Spring 2024</h1>
+            <h2>May 9, 2024</h2>
+            <h2>Developers</h2>
+            <div id="developers" class="row row-cols-2">
+                <div id="developerContainer">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h3>Luke Pedersen | lukeped@iastate.edu</h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="developerContainer">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h3>Rachel San Agustin | rds2@iastate.edu</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2>Intructors</h2>
+            <div id="instructors" class="row row-cols-2">
+                <div id="developerContainer">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h3>Dr. Abraham N. Aldaco Gastelum | aaldaco@iastate.edu</h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="developerContainer">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h3>Dr. Ali Jannesari | jannesar@iastate.edu</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+export default DeveloperInfo;
