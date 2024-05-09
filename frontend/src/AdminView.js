bfeade158d22f266eb8afa72d4e1f3f8ee52e72b
racheldesa <racheldesa@outlook.com>
2024-05-08 20:00:46 -0500

    Started creating form to update grades

--- frontend/src/AdminView.js
+++ frontend/src/AdminView.js
@@ -28,9 +28,29 @@ function AdminView() {
         )
     }   // end Login()
 
+    function UpdateGrades() {
+        const onSubmit = data => {
+            setDataF(data);
+            setViewer(2);
+        }
+        return (
+            <div>
+                <h1>Update Grades</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("gradebookEntry", { required: true })} placeholder="Gradebook Entry" />
+                    {errors.gradebookEntry && <p>Gradebook entry title required</p>}
+                    <input {...register("stuID", { required: true })} placeholder="Student ID" />
+                    {errors.stuID && <p>Student ID required.</p>}
+                    <button type="submit">Update</button>
+                </form>
+            </div>
+        )
+    }
+
     return (
         <div>
             {viewer===0 && <Login />}
+            {viewer===1 && <UpdateGrades />}
         </div>
     );  // end AdminView
 }
